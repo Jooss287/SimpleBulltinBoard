ef1e7a042237f69064a4de54fba3fdc9eb272f9a
jooss287 <jooss287@gmail.com>
2021-05-10 23:19:41 +0900

    initialize gradle project

---
+++ src/main/java/Example/SimpleBulltinBoard/SimpleBulltinBoardApplication.java
@@ -0,0 +1,13 @@
+package Example.SimpleBulltinBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBulltinBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBulltinBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Example/SimpleBulltinBoard/SimpleBulltinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package Example.SimpleBulltinBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBulltinBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
